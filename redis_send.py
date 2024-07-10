1e0665eea5065bf6a51ee8e1a4d4caabcf7ff9ec
Nguyen Cao Dien <118111366+CaoDien2003@users.noreply.github.com>
2024-07-10 15:30:38 +0700

    redis

--- redis_send.py
+++ redis_send.py
@@ -1,7 +1,3 @@
-import redis
-
-# Connect to the Redis server
-r = redis.Redis(host='192.168.80.74', port=6379, password='12345')
-
-# Publish the command to the channel
-r.publish('start_hadoop', 'sh /path/to/hadoop/start_script.sh')
+import subprocess
+def start_client():
+   subprocess.run(["docker", "run",  "--name", "some-mongo", "-d", "mongo:latest"])
