6ae2525be1f1c8b29b82cda963c1204d61c873b8
vpa1412 <vpa141203@gmail.com>
2024-07-16 14:04:58 +0700

    changes

--- stream.py
+++ stream.py
@@ -7,7 +7,7 @@ from pyspark.ml import PipelineModel
 
 
 
-spark = SparkSession.builder.appName("Cassandra")\
+spark = SparkSession.builder.appName("KafkaDataProcessing")\
   .config('spark.cassandra.connection.host', '127.0.0.1:9042')\
   .getOrCreate()
 
@@ -23,34 +23,34 @@ spark.sparkContext.setLogLevel('ERROR')
 
 
 schema = StructType([
-    StructField("ID", StringType(), True),
-    StructField("Age", FloatType(), True),  # Age is a float type
-    StructField("Annual_Income", FloatType(), True),  # Annual_Income is a float type
-    StructField("Monthly_Inhand_Salary", FloatType(), True),  # Monthly_Inhand_Salary is a float type
-    StructField("Num_Bank_Accounts", IntegerType(), True),  # Num_Bank_Accounts is an integer type
-    StructField("Num_Credit_Card", IntegerType(), True),  # Num_Credit_Card is an integer type
-    StructField("Interest_Rate", IntegerType(), True),  # Interest_Rate is an integer type
-    StructField("Num_of_Loan", FloatType(), True),  # Num_of_Loan is a float type
-    StructField("Delay_from_due_date", IntegerType(), True),  # Delay_from_due_date is an integer type
-    StructField("Num_of_Delayed_Payment", FloatType(), True),  # Num_of_Delayed_Payment is a float type
-    StructField("Changed_Credit_Limit", FloatType(), True),  # Changed_Credit_Limit is a float type
-    StructField("Num_Credit_Inquiries", FloatType(), True),  # Num_Credit_Inquiries is a float type
-    StructField("Outstanding_Debt", FloatType(), True),  # Outstanding_Debt is a float type
-    StructField("Credit_Utilization_Ratio", FloatType(), True),  # Credit_Utilization_Ratio is a float type
-    StructField("Total_EMI_per_month", FloatType(), True),  # Total_EMI_per_month is a float type
-    StructField("Amount_invested_monthly", FloatType(), True),  # Amount_invested_monthly is a float type
-    StructField("Monthly_Balance", FloatType(), True),  # Monthly_Balance is a float type
-    StructField("Occupation_Numeric", IntegerType(), True),  # Occupation_Numeric is an integer type
-    StructField("Credit_History_Age_Numeric", IntegerType(), True),  # Credit_History_Age_Numeric is an integer type
-    StructField("Payment_of_Min_Amount_Numeric", IntegerType(), True),  # Payment_of_Min_Amount_Numeric is an integer type
-    StructField("Payment_Behaviour_Numeric", IntegerType(), True)  # Payment_Behaviour_Numeric is an integer type
+    StructField("ID", StringType()),
+    StructField("Age", StringType()),  # Age is a float type
+    StructField("Annual_Income", StringType()),  # Annual_Income is a float type
+    StructField("Monthly_Inhand_Salary", StringType()),  # Monthly_Inhand_Salary is a float type
+    StructField("Num_Bank_Accounts", StringType()),  # Num_Bank_Accounts is an integer type
+    StructField("Num_Credit_Card", StringType()),  # Num_Credit_Card is an integer type
+    StructField("Interest_Rate", StringType()),  # Interest_Rate is an integer type
+    StructField("Num_of_Loan", StringType()),  # Num_of_Loan is a float type
+    StructField("Delay_from_due_date", StringType()),  # Delay_from_due_date is an integer type
+    StructField("Num_of_Delayed_Payment", StringType()),  # Num_of_Delayed_Payment is a float type
+    StructField("Changed_Credit_Limit", StringType()),  # Changed_Credit_Limit is a float type
+    StructField("Num_Credit_Inquiries", StringType()),  # Num_Credit_Inquiries is a float type
+    StructField("Outstanding_Debt", StringType()),  # Outstanding_Debt is a float type
+    StructField("Credit_Utilization_Ratio", StringType()),  # Credit_Utilization_Ratio is a float type
+    StructField("Total_EMI_per_month", StringType()),  # Total_EMI_per_month is a float type
+    StructField("Amount_invested_monthly", StringType()),  # Amount_invested_monthly is a float type
+    StructField("Monthly_Balance", StringType()),  # Monthly_Balance is a float type
+    StructField("Occupation_Numeric", StringType()),  # Occupation_Numeric is an integer type
+    StructField("Credit_History_Age_Numeric", StringType()),  # Credit_History_Age_Numeric is an integer type
+    StructField("Payment_of_Min_Amount_Numeric", StringType()),  # Payment_of_Min_Amount_Numeric is an integer type
+    StructField("Payment_Behaviour_Numeric", StringType())  # Payment_Behaviour_Numeric is an integer type
 ])
 
 
 df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "192.168.80.83:9092") \
-    .option("subscribe", "credit_card") \
+    .option("subscribe", "credit_testing") \
     .option("startingOffsets", "latest") \
     .option("failOnDataLoss", False) \
     .load() \
@@ -87,7 +87,7 @@ df_qom = df.filter("ID is not null") \
 df_qom.writeStream \
     .trigger(processingTime="10 seconds") \
     .format("console") \
-    .option("checkpointLocation", "/home/ktinh/checkpoint1") \
+    .option("checkpointLocation", "/home/phuonganh/CS411/checkpoint3") \
     .outputMode("update") \
     .start()
 
@@ -95,3 +95,4 @@ df_qom.writeStream \
 
 spark.streams.awaitAnyTermination()
 spark.stop()
+
